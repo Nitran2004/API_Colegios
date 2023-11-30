cea3f64ef41ff469f0421626d108fb0ba3c32218
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-11-30 14:00:08 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/CentroCostosController.cs
@@ -0,0 +1,62 @@
+﻿using Avance.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Avance.Controllers
+{
+    public class CentroCostosController : Controller
+    {
+        public async Task<IActionResult> CentroCostos()
+        {
+            string url = "http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    var centroCostos = JsonSerializer.Deserialize<IEnumerable<CentroCostos>>(data);
+                    return View(centroCostos);
+                }
+                else
+                {
+                    return View("Exception", new { message = "Error retrieving data" });
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PushCentroCostos(string codigo, string descripcion)
+        {
+            try
+            {
+                string url = $"http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosInsert?codigocentrocostos={codigo}&descripcioncentrocostos={descripcion}";
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("CentroCostos");
+                    }
+                    else
+                    {
+                        return View("Exception", new { message = "Error al insertar el centro de costos" });
+                    }
+                }
+            }
+            catch
+            {
+                return View("Exception", new { message = "Error al insertar el centro de costos" });
+            }
+        }
+
+        public IActionResult AddCentroDeCostos()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Avance.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Avance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            string url = "http://apiservicios.ecuasolmovsa.com:3009/api/Varios/GetEmisor";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    var emisores = JsonSerializer.Deserialize<IEnumerable<EmisorModel>>(data);
+
+                    var nombresEmisores = emisores.Select(e => e.NombreEmisor); // Obtain only the names of the emisores
+
+                    return View("Index", nombresEmisores);
+                }
+                else
+                {
+                    return View("Exception", new { message = "Error retrieving data" });
+                }
+            }
+        }
+    }
+}
+
+
+
